40808a9db51111278c603291b7ae5a481de96046
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2023-04-25 19:56:15 -0300

    Aula 16 ao 20

--- aula15.py
+++ aula15.py
@@ -7,4 +7,4 @@ numero_2 = input('Digite outro número: ')
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
-print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+print(f'A soma dos números é: "{int_numero_1 + int_numero_2}"')
